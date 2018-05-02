26ebb0c9e2f8a9abd9b2560fe06cc3a143f1bb7d
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-05-02 08:19:43 +0100

    use-calculator.js

--- use-calculator.js
+++ use-calculator.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 let parser = require('public/calculator.js');
 
-let r = parser.parse(process.argv.slice(2).pop() || "2*3");
+let input =process.argv.slice(2).pop() || "2*3";
+let r = parser.parse(input);
 console.log(r);
